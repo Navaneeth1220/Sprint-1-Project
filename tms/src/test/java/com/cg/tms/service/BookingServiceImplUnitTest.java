73fcba75d8486adfb9f380a2dca1c007403963e3
Navaneeth S <nava8092@gmail.com>
2021-03-24 01:07:59 +0530

    proper project structure

---
+++ tms/src/test/java/com/cg/tms/service/BookingServiceImplUnitTest.java
@@ -0,0 +1,14 @@
+package com.cg.tms.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookingServiceImplUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
