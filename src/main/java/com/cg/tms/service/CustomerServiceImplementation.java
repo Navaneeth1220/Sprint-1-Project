4b50c6dfb38abcf6f3bf09939cfaaf00c45ff503
unknown <hokkaidutt@gmail.com>
2021-03-27 14:44:26 +0530

    Test case Added

--- src/main/java/com/cg/tms/service/CustomerServiceImplementation.java
+++ src/main/java/com/cg/tms/service/CustomerServiceImplementation.java
@@ -12,69 +12,83 @@ import com.cg.tms.exceptions.PackageNotFoundException;
 import com.cg.tms.exceptions.RouteNotFoundException;
 import com.cg.tms.repository.ICustomerRepository;
 import com.cg.tms.exceptions.*;
+
 @Service
-public class CustomerServiceImplementation implements ICustomerService{
-@Autowired
-ICustomerRepository repo;
-	@Override
-	
-	public Customer addCustomer(Customer customer) {
-		validateId(customer.getCustomerId());
-		validateCustomerName(customer.getCustomerName());
-		validateCustomerAddress(customer.getAddress());
-		repo.save(customer);
-        
-		return customer;
-	}
-
-	public void validateCustomerAddress(String address) {
-
-		
-	}
-
-	public void validateCustomerName(String customerName) {
-
-	}
-
-	public void validateId(int customerId) {
-			if (customerId < 0) {
-				throw new InvalidIdException("ID cannot be negative");
-			}
-		}
+public class CustomerServiceImplementation implements ICustomerService {
+    @Autowired
+    private ICustomerRepository repo;
 
 
-	@Override
-	public Customer updateCustomer(Customer customer) throws CustomerNotFoundException {
+    @Override
+    public Customer addCustomer(Customer customer) {
+        validateCustomer(customer);
+        Customer saved=repo.save(customer);
+        return saved;
+    }
 
-		return null;
-	}
+    public void validateCustomerAddress(String address) {
 
-	@Override
-	public Customer deleteCustomer(Customer customer) throws CustomerNotFoundException {
-		
-		return null;
-	}
 
-	@Override
-	public Customer viewCustomer(int custid) throws CustomerNotFoundException {
-		Optional<Customer> optional = repo.findById(custid);
-		if(!optional.isPresent()) {
-			throw new CustomerNotFoundException("Booking not found");
-		}
-		return optional.get();
-	}
-		
+    }
 
-	@Override
-	public List<Customer> viewAllCustomers(int packageId) throws PackageNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void validateCustomerName(String customerName) {
 
-	@Override
-	public List<Customer> viewCustomerList(int routeId) throws RouteNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    }
+
+    public void validateId(int customerId) {
+        if (customerId < 0) {
+            throw new InvalidIdException("ID cannot be negative");
+        }
+    }
+
+
+    @Override
+    public Customer updateCustomer(Customer customer) throws CustomerNotFoundException {
+
+        return null;
+    }
+
+    @Override
+    public Customer deleteCustomer(int customerId) throws CustomerNotFoundException {
+
+    		validateId(customerId);
+    		Optional<Customer> optional = repo.findById(customerId);
+    		if (!optional.isPresent()) {
+    			throw new CustomerNotFoundException("Customer not found");
+    		}
+    		Customer fetched=optional.get();
+    		repo.delete(fetched);
+    		return fetched;
+    	}
+    }
+
+    @Override
+    public Customer viewCustomer(int custid) throws CustomerNotFoundException {
+        Optional<Customer> optional = repo.findById(custid);
+        if (!optional.isPresent()) {
+            throw new CustomerNotFoundException("Customer not found");
+        }
+        return optional.get();
+    }
+
+
+    @Override
+    public List<Customer> viewAllCustomers(int packageId) throws PackageNotFoundException {
+
+        return null;
+    }
+
+    @Override
+    public List<Customer> viewCustomerList(int routeId) throws RouteNotFoundException {
+
+        return null;
+    }
+
+    void validateCustomer(Customer customer){
+        validateId(customer.getCustomerId());
+        validateCustomerName(customer.getCustomerName());
+        validateCustomerAddress(customer.getAddress());
+
+    }
 
 }
